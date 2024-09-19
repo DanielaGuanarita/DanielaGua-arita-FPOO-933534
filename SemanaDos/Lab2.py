bb05516f75b0b15de75ade8598be26671f180c77
Daniela Guañarita <leidy.guanarita@uniminuto.edu.co>
2024-09-19 00:08:23 +0000

    creando una cclase

---
+++ SemanaDos/Lab2.py
@@ -0,0 +1,37 @@
+person = {
+    "name": "Daniela",
+    "age": 30
+
+}
+print(person["age"])
+
+product 
+
+id: int
+name: str
+price: float
+section: int
+
+1. tech
+2.Clothes
+3.Food
+
+store =[]
+
+
+id = int(input('Por favor ingrese el id de su producto:\n))
+name = input('Por favor ingrese el nombre de su producto:\n')
+price = float(input('Por favor ingrese el precio de su producto:\n'))
+section = input('Por favor ingrese la section para su producto:\n')
+
+product1 = {
+"id": id,
+"name": name,
+"price": price,
+"section": section,
+}
+store.append(product1)
+
+#mostrar los productos del almacen 
+for product in store
+    print(product)
