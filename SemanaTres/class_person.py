f736384daf035ecf253da18cdb6a7d541a7e42fa
Daniela Guañarita <leidy.guanarita@uniminuto.edu.co>
2024-09-26 01:43:49 +0000

    Haciendo una clase

--- SemanaTres/class_person.py
+++ SemanaTres/class_person.py
@@ -15,7 +15,18 @@ class Person:
         self.role = role
 
     def __repr__(self):
-      return f"{self.name} {self.lastname}"
+      return f"{self.dni} {self.name} {self.lastname} {self.role}"
+
     
 person1 = Person(dni=123, name="Luisito", lastname="Velez", role=1)
+person2 = Person(dni=1211, name="Lady", lastname="Rodas", role=1)
+person3 = Person(dni=2415, name="Julian", lastname="Muñoz", role=1)
+person4 = Person(dni=7852, name="Silvana", lastname="Bolaños", role=1)
+
+
 print(person1)
+print(person2)
+print(person3)
+print(person4)
+
+
