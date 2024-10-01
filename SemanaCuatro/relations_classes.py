58d87b7c465e61ca72fb54e5f8343c0909ff0e84
Daniela Guañarita <leidy.guanarita@uniminuto.edu.co>
2024-10-01 02:31:40 +0000

    relaciond de clases

---
+++ SemanaCuatro/relations_classes.py
@@ -0,0 +1,45 @@
+class Author:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def add_books(self, book: 'Book'):
+        self.books.append(book)
+
+    def get_books(self):
+        return f"Libros del autor = {[book.title for book in self.books]}"
+
+#------------------------------------
+
+class Book:
+    def __init__(self, title: str, pages: int, author: Author):
+        self.title = title
+        self.pages = pages
+        self.Author = author
+
+    def __str__(self):
+        return f"Libro: {self.title}"
+
+
+
+
+#------------------------------------
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+
+
+
+author_1 = Author(name='Gabriel Garcia Marquez')
+book_1 = Book('Cien años de soledad', 473, author_1)
+
+author_1.add_books(book_1)
+
+
+#Llamado atributos de clase
+
+print(author_1.book)
+
+
